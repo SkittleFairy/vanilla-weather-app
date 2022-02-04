dcac4df5e5f1cb69d667364dce0edbbf317ea731
SkittleFairy <95044797+SkittleFairy@users.noreply.github.com>
2022-02-03 23:39:51 -0500

    Showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innterHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "e867e7636933b2cd7b9f5822ae56c4a1";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Norcross&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
