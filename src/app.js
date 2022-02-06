c6c20d56d8f8f1d7a1ead8a283cd3d08b3bd687d
SkittleFairy <95044797+SkittleFairy@users.noreply.github.com>
2022-02-06 16:45:56 -0500

    Added search engine

--- src/app.js
+++ src/app.js
@@ -58,8 +58,20 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "e867e7636933b2cd7b9f5822ae56c4a1";
-let city = "Norcross";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "e867e7636933b2cd7b9f5822ae56c4a1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("New York");
